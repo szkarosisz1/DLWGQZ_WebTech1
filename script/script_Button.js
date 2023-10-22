d7e96aed49e582624a0807ac67ed8d183bcd66ae
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2023-10-22 11:36:53 +0200

    Add files via upload

---
+++ script/script_Button.js
@@ -0,0 +1,80 @@
+//vásárlás gomb kezelése a Telefon.html-nél
+var xiaomiPhoneShoppingClick = document.getElementById("btn_shoppingClickPhone_1");
+var iphonePhoneShoppingClick = document.getElementById("btn_shoppingClickPhone_2");
+var samsungPhonetShoppingClick = document.getElementById("btn_shoppingClickPhone_3");
+
+
+function shoppingClickPhone1() {
+    xiaomiPhoneShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    xiaomiPhoneShoppingClick.style.color = 'whitesmoke';
+    xiaomiPhoneShoppingClick.style.fontWeight = 'bolder';
+}
+
+function shoppingClickPhone2() {
+    iphonePhoneShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    iphonePhoneShoppingClick.style.color = 'whitesmoke';
+    iphonePhoneShoppingClick.style.fontWeight = 'bolder';  
+}
+
+function shoppingClickPhone3() {
+    samsungPhonetShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    samsungPhonetShoppingClick.style.color = 'whitesmoke';
+    samsungPhonetShoppingClick.style.fontWeight = 'bolder';
+}
+
+
+//**************************************************************************************
+
+//vásárlás gomb kezelése a Tablet.html-nél
+var ipadTabletShoppingClick = document.getElementById("btn_shoppingClickTablet_1");
+var samsungTabletShoppingClick = document.getElementById("btn_shoppingClickTablet_2");
+var xiaomiTabletShoppingClick = document.getElementById("btn_shoppingClickTablet_3");
+
+
+function shoppingClickTablet1() {
+    ipadTabletShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    ipadTabletShoppingClick.style.color = 'whitesmoke';
+    ipadTabletShoppingClick.style.fontWeight = 'bolder';
+}
+
+function shoppingClickTablet2() {
+    samsungTabletShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    samsungTabletShoppingClick.style.color = 'whitesmoke';
+    samsungTabletShoppingClick.style.fontWeight = 'bolder';  
+}
+
+function shoppingClickTablet3() {
+    xiaomiTabletShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    xiaomiTabletShoppingClick.style.color = 'whitesmoke';
+    xiaomiTabletShoppingClick.style.fontWeight = 'bolder';
+}
+
+//**************************************************************************************
+
+//vásárlás gomb kezelése a Laptop.html-nél
+var asusLaptopShoppingClick = document.getElementById("btn_shoppingClick_1");
+var hpLaptopShoppingClick = document.getElementById("btn_shoppingClick_2");
+var macLaptopShoppingClick = document.getElementById("btn_shoppingClick_3");
+var dellLaptopShoppingClick = document.getElementById("btn_shoppingClick_4");
+
+function shoppingClick1() {
+    asusLaptopShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    asusLaptopShoppingClick.style.color = 'whitesmoke';
+    asusLaptopShoppingClick.style.fontWeight = 'bolder';      
+}
+
+function shoppingClick2() {
+    hpLaptopShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    hpLaptopShoppingClick.style.color = 'whitesmoke';
+    hpLaptopShoppingClick.style.fontWeight = 'bolder';    
+}
+function shoppingClick3() {
+    macLaptopShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    macLaptopShoppingClick.style.color = 'whitesmoke';
+    macLaptopShoppingClick.style.fontWeight = 'bolder';    
+}
+function shoppingClick4() {
+    dellLaptopShoppingClick.innerHTML = "A vásárlás megtörtént.";
+    dellLaptopShoppingClick.style.color = 'whitesmoke'; 
+    dellLaptopShoppingClick.style.fontWeight = 'bolder';   
+}
